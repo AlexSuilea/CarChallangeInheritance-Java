ec53a7b4bcdfad9881c4e7380a8ed907366bc49a
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-14 01:49:15 +0300

    Add files via upload

---
+++ CarChallangeInheritance/src/com/alexsuilea/BMW.java
@@ -0,0 +1,29 @@
+package com.alexsuilea;
+
+public class BMW extends Car{
+    private int roadServiceMonths;
+
+    public BMW(int roadServiceMonths) {
+        super("BMW", "F30", 5, 4, 6, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate){
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0){
+            stop();
+            changeGear(1);
+        } else if(newVelocity > 0 && newVelocity <= 10){
+            changeGear(1);
+        } else if(newVelocity > 10 && newVelocity <= 20){
+            changeGear(2);
+        } else if(newVelocity > 20 && newVelocity <= 30){
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+        if(newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentVelocity());
+        }
+    }
+}
